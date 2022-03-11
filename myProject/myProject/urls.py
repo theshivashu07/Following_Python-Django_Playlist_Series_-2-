4c075cd6967d8e956cc9d3c58fb4a6c28178d629
Shivam Shukla <shivamshukla3108@gmail.com>
2022-03-11 20:18:09 +0530

    Lecture 12

--- myProject/myProject/urls.py
+++ myProject/myProject/urls.py
@@ -17,5 +17,13 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-panel/', admin.site.urls),     # path('admin/', admin.site.urls),
 ]
+
+
+
+
+
+
+
+
