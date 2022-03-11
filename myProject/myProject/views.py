106a696aa8a08cf60ac221ce238e44b88fb5f9a9
Shivam Shukla <shivamshukla3108@gmail.com>
2022-03-12 03:18:26 +0530

    Lecture 16

--- myProject/myProject/views.py
+++ myProject/myProject/views.py
@@ -1,14 +1,20 @@
 
-from django.http import HttpResponse 
+from django.http import HttpResponse  
 from django.shortcuts import render
 
 
 def homepage(request):
-    return render(request,"index.html")
+    data={
+        'title' : 'Home Page',
+        'message' : 'Welcome to shivashu.com!',
+        'mylist' : ['shivam','vashu','shivashu','shrivashu'],
+    }
+    return render(request,'index.html',data);
+
 def aboutus(request):
-        return HttpResponse("<b>Welcome to shivashu.com</b>")
+    return HttpResponse("<b>Welcome to shivashu.com</b>");
 def course(request):
-        return HttpResponse("Welcome to Python")
+    return HttpResponse("Welcome to Python");
 def courseDetails(request,courseid):
     return HttpResponse(courseid)
 
