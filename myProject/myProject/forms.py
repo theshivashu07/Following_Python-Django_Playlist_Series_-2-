9ed48c01f21a7a3ddb40c85e2ca162b33ed01435
Shivam Shukla <shivamshukla3108@gmail.com>
2022-03-15 14:29:24 +0530

    Lecture 29 2.0

--- myProject/myProject/forms.py
+++ myProject/myProject/forms.py
@@ -2,9 +2,11 @@
 from django import forms
 
 class userForm(forms.Form):
-	firstvalue=forms.CharField()
-	secondvalue=forms.CharField()
-
+	# firstvalue=forms.CharField(label="First Value", required=False, widget=forms.TextInput(attrs={'class':"form-control"}))
+	# secondvalue=forms.CharField(label="First Value", required=False, widget=forms.TextInput(attrs={'class':"form-control"}))
+	firstvalue=forms.CharField(label="First Value", widget=forms.TextInput(attrs={'class':"form-control"}))
+	secondvalue=forms.CharField(label="First Value", widget=forms.TextInput(attrs={'class':"form-control"}))
+	# email=forms.EmailField()
 
 
 
