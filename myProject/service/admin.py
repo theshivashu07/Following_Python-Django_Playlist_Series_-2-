c740b41873c2371254b6ccafdf42b3db2b409c4d
Shivam Shukla <shivamshukla3108@gmail.com>
2022-03-16 18:32:55 +0530

    Lecture 35

--- myProject/service/admin.py
+++ myProject/service/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from service.models import Service
 # Register your models here.
+
+class ServiceAdmin(admin.ModelAdmin):
+	list_display=('service_icon','service_title', 'service_des')
+admin.site.register(Service,ServiceAdmin)
+
+"""
+# We also pass this, but then one last field is hidden,
+# And whenever we want it back, so we again adding this.
+class ServiceAdmin(admin.ModelAdmin):
+	list_display=('service_icon','service_title')
+admin.site.register(Service,ServiceAdmin)
+"""
+
+
+
+
+
